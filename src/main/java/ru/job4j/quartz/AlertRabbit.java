79d3c63ae4576d26ed7046cd3de19bb26ea8db8a
Carcharodon80 <karna241280@gmail.com>
2021-12-16 12:33:51 +0300

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,55 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        AlertRabbit alertRabbit = new AlertRabbit();
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(alertRabbit.getIntervalFromProperties(alertRabbit.getPropertiesFromFile()))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    private Properties getPropertiesFromFile() {
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream("rabbit.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    private int getIntervalFromProperties(Properties properties) {
+        int interval;
+            interval = Integer.parseInt(properties.getProperty("rabbit.interval", "10"));
+        return interval;
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here...");
+        }
+    }
+}
