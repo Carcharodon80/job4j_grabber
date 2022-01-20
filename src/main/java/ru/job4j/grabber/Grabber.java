b80bb47ddb731d32f50db84228229e87f7b1bcfe
Carcharodon80 <karna241280@gmail.com>
2022-01-20 23:41:59 +0300

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -8,6 +8,9 @@ import ru.job4j.html.SqlRuParse;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.Properties;
 
@@ -17,6 +20,7 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 /**
  * 6. Grabber. [#289477]
+ * 7. Web. [#289478]
  */
 public class Grabber implements Grab {
     private final Properties cfg = new Properties();
@@ -44,6 +48,27 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK/r/n/r/n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     @Override
     public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
         JobDataMap data = new JobDataMap();
@@ -64,7 +89,7 @@ public class Grabber implements Grab {
 
     public static class GrabJob implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             JobDataMap map = context.getJobDetail().getJobDataMap();
             Store store = (Store) map.get("store");
             Parse parse = (Parse) map.get("parse");
@@ -83,6 +108,7 @@ public class Grabber implements Grab {
         Parse parse = new SqlRuParse(new SqlRuDateTimeParser());
         try {
             grab.init(parse, store, scheduler);
+            grab.web(store);
         } catch (SchedulerException e) {
             e.printStackTrace();
         }
